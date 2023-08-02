5613623375f2ba7d503573882bb34a45ca91de0d
regiskeller1 <regiskeller17@gmail.com>
2023-08-02 18:08:02 -0300

    arrumando chaves

--- src/SmartTv.java
+++ src/SmartTv.java
@@ -4,26 +4,32 @@ public class SmartTv {
     int volume = 25;
 
     public void ligarTv(){
-         this.ligada = true;
+
+        this.ligada = true;
     }
 
     public void desligarTv(){
+
         this.ligada = false;
     }
 
     public void proximoCanal(){
+
         this.canal++;
     }
 
     public void aumentarVolume(){
+
         this.volume++;
     }
 
     public void diminuirVolume(){
+
         this.volume--;
     }
 
     public void alterarCanal(int canal){
+
         this.canal = canal;
     }
 
